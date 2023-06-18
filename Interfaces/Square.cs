0e10c8ed07ba967f9c89b4874d948771bda74b5e
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-06-18 14:46:55 -0800

    Iterfaces and Exaption

--- Interfaces/Square.cs
+++ Interfaces/Square.cs
@@ -2,7 +2,26 @@
 
 namespace ExceptionInterfaces.Interfaces
 {
-    internal class Square:
+    internal class Square : IShape
     {
+        void IDrawable.Draw()
+        {
+            Console.WriteLine("Drawing *** ...");
+        }
+
+        void IPrintable.Draw()
+        {
+            Console.WriteLine("Drawing  Square ...");
+        }
+
+        int IShape.GetNumberOfSides()
+        {
+            return 8;
+        }
+
+        void IPrintable.Print()
+        {
+            Console.WriteLine("Printing Square ...");
+        }
     }
 }
